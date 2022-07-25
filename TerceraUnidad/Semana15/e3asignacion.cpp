ad0a9ef72833e61cd9d82a4ddb7b9a8f6b401504
Alejandro <52181711+VictorQuicano@users.noreply.github.com>
2022-07-25 13:56:00 -0500

    Ejer 3 final

--- TerceraUnidad/Semana15/e3asignacion.cpp
+++ TerceraUnidad/Semana15/e3asignacion.cpp
@@ -1,4 +1,34 @@
 #include "linked_list/LinkedList.h"
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+std::istream& operator>>(std::istream &is, LinkedList& cu){
+    std::cout << "Cuantos valores ingresara?\n >";
+    int a{0};
+    if (not(is>>a)){
+        is.setstate(std::ios::failbit);
+        return is;
+    }
+    else{
+        for (int i = 0; i < a;i++){
+            int b{0};
+            cout << "# " << i + 1 << " > ";
+            cin >> b;
+            if(!cin){
+                cout << "No es un entero" << endl;
+                cin.clear();
+                cin.ignore();
+            }
+            else{
+                cu.insert(b);
+            }
+        }
+        return is;
+    }
+}
+int main(){
+    LinkedList l1(4);
+    cin >> l1;
+    cout << l1;
+}
